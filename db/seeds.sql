836b1eb887c84b6a496c68d481868dc469ffdb13
Christen McFarland <Christen McFarland>
2017-10-23 18:44:12 -0400

    Current Progress

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('Steak & Onion', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Double Decker', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Bacon & Egg', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Vegan Black Bean', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Bunless', false);
